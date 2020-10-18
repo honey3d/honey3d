2d046ffd16d8ff3a06f92ca438ca6b2d6842ce6a
sanine-a <sanine.not@pm.me>
2020-10-18 12:21:38 -0500

    add LICENSE.md

--- demo/main.lua
+++ demo/main.lua
@@ -2,7 +2,10 @@ for key, value in pairs(honey.input.key) do
    print(key, value)
 end
 
+local total_time = 0
+
 function honey.update(dt)
+   total_time = total_time + dt
    print(dt)
 end
 
--- include/error.h
+++
@@ -1,7 +0,0 @@
-#ifndef HONEY_ERROR_H
-#define HONEY_ERROR_H
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-  
-#endif
