f9f790c5cd3de6cd82ebf08c1f1695d411dd4a60
sanine <sanine.not@pm.me>
2021-09-23 23:38:45 -0500

    add honey_debug_()

--- src/logging/logging.h
+++ src/logging/logging.h
@@ -1,10 +1,22 @@
 #ifndef HONEY_LOGGING_H
 #define HONEY_LOGGING_H
 
+#include <stdio.h>
+
+
 struct honey_log_info {
    enum { DEBUG, INFO, WARN, ERROR, FATAL } log_level;
+   FILE *debug_out;
+   FILE *info_out;
+   FILE *warn_out;
+   FILE *error_out;
+   FILE *fatal_out;
 };
 
 const char * honey_log_level_str_(struct honey_log_info *info);
+
+#define honey_debug_(info, ...) \
+   fprintf(info.debug_out, "[DEBUG] " __VA_ARGS__)
+   
 
 #endif
--- src/test/logging/logging_tests.c
+++ src/test/logging/logging_tests.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "test/minunit.h"
 #include "test/suites.h"
 
@@ -7,6 +10,7 @@
 /* test declarations */
 
 mu_test test_log_get_level();
+mu_test test_log_debug();
 
 
 /* main suite */
@@ -14,6 +18,7 @@ mu_test test_log_get_level();
 void honey_logging_tests()
 {
    mu_run_test("get log level strings", test_log_get_level);
+   mu_run_test("print debug message", test_log_debug);
 }
 
 
@@ -37,6 +42,27 @@ mu_test test_log_get_level()
 
    info.log_level = FATAL;
    mu_assert_streq("FATAL", honey_log_level_str_(&info));
+
+   return 0;
+}
+
+
+mu_test test_log_debug()
+{
+   FILE *stream;
+   char *buffer;
+   size_t len;
+
+   stream = open_memstream(&buffer, &len);
+   mu_assert_unequal(stream, NULL);
+
+   struct honey_log_info info;
+   info.debug_out = stream;
+
+   honey_debug_(info, "hello, world!");
+   fclose(stream);
+   mu_assert_streq(buffer, "[DEBUG] hello, world!");
+   free(buffer);
 
    return 0;
 }
