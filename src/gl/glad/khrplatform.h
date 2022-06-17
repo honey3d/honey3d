38fc53f3ddb1e27457307dc8ac34c0d5e535a9c0
sanine <sanine.not@pm.me>
2022-06-16 22:19:51 -0500

    fix bad glad files

--- src/gl/glad/khrplatform.h
+++ src/gl/glad/khrplatform.h
@@ -1,332 +1,4 @@
-<!DOCTYPE html>
-<html lang='en'>
-<head>
-<title>khrplatform.h\glad\src - honey-engine - a 3D game engine written in C</title>
-<meta name='generator' content='cgit v1.2.1'/>
-<meta name='robots' content='index, nofollow'/>
-<link rel='stylesheet' type='text/css' href='/cgit.css'/>
-<link rel='shortcut icon' href='/favicon.svg'/>
-<link rel='alternate' title='Atom feed' href='https://sanine.net/git/honey-engine/atom/src/glad/khrplatform.h?h=main' type='application/atom+xml'/>
-<link rel='vcs-git' href='https://sanine.net/git/honey-engine' title='honey-engine Git repository'/>
-</head>
-<body>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <div id="navigation"><pre><a href="/index.html">home</a>   <a href="/projects/">projects</a>   <a href="/git">git</a>   <a href="/about.html">about</a></pre></div>
-<div id='cgit'><table id='header'>
-<tr>
-<td class='logo' rowspan='2'><a href='/git/'><img src='/cgit.png' alt='cgit logo'/></a></td>
-<td class='main'><a href='/git/'>index</a> : <a title='honey-engine' href='/git/honey-engine/'>honey-engine</a></td><td class='form'><form method='get'>
-<select name='h' onchange='this.form.submit();'>
-<option value='main' selected='selected'>main</option>
-<option value='refactor-tdd'>refactor-tdd</option>
-</select> <input type='submit' value='switch'/></form></td></tr>
-<tr><td class='sub'>a 3D game engine written in C</td><td class='sub right'></td></tr></table>
-<table class='tabs'><tr><td>
-<a href='/git/honey-engine/'>summary</a><a href='/git/honey-engine/refs/'>refs</a><a href='/git/honey-engine/log/src/glad/khrplatform.h'>log</a><a class='active' href='/git/honey-engine/tree/src/glad/khrplatform.h'>tree</a><a href='/git/honey-engine/commit/src/glad/khrplatform.h'>commit</a><a href='/git/honey-engine/diff/src/glad/khrplatform.h'>diff</a></td><td class='form'><form class='right' method='get' action='/git/honey-engine/log/src/glad/khrplatform.h'>
-<select name='qt'>
-<option value='grep'>log msg</option>
-<option value='author'>author</option>
-<option value='committer'>committer</option>
-<option value='range'>range</option>
-</select>
-<input class='txt' type='search' size='10' name='q' value=''/>
-<input type='submit' value='search'/>
-</form>
-</td></tr></table>
-<div class='path'>path: <a href='/git/honey-engine/tree/'>root</a>/<a href='/git/honey-engine/tree/src'>src</a>/<a href='/git/honey-engine/tree/src/glad'>glad</a>/<a href='/git/honey-engine/tree/src/glad/khrplatform.h'>khrplatform.h</a></div><div class='content'>blob: 5b55ea2b9814c22457f20e8b6bbe4c3b4579147e (<a href='/git/honey-engine/plain/src/glad/khrplatform.h'>plain</a>)
-<table summary='blob content' class='blob'>
-<tr><td class='linenumbers'><pre><a id='n1' href='#n1'>1</a>
-<a id='n2' href='#n2'>2</a>
-<a id='n3' href='#n3'>3</a>
-<a id='n4' href='#n4'>4</a>
-<a id='n5' href='#n5'>5</a>
-<a id='n6' href='#n6'>6</a>
-<a id='n7' href='#n7'>7</a>
-<a id='n8' href='#n8'>8</a>
-<a id='n9' href='#n9'>9</a>
-<a id='n10' href='#n10'>10</a>
-<a id='n11' href='#n11'>11</a>
-<a id='n12' href='#n12'>12</a>
-<a id='n13' href='#n13'>13</a>
-<a id='n14' href='#n14'>14</a>
-<a id='n15' href='#n15'>15</a>
-<a id='n16' href='#n16'>16</a>
-<a id='n17' href='#n17'>17</a>
-<a id='n18' href='#n18'>18</a>
-<a id='n19' href='#n19'>19</a>
-<a id='n20' href='#n20'>20</a>
-<a id='n21' href='#n21'>21</a>
-<a id='n22' href='#n22'>22</a>
-<a id='n23' href='#n23'>23</a>
-<a id='n24' href='#n24'>24</a>
-<a id='n25' href='#n25'>25</a>
-<a id='n26' href='#n26'>26</a>
-<a id='n27' href='#n27'>27</a>
-<a id='n28' href='#n28'>28</a>
-<a id='n29' href='#n29'>29</a>
-<a id='n30' href='#n30'>30</a>
-<a id='n31' href='#n31'>31</a>
-<a id='n32' href='#n32'>32</a>
-<a id='n33' href='#n33'>33</a>
-<a id='n34' href='#n34'>34</a>
-<a id='n35' href='#n35'>35</a>
-<a id='n36' href='#n36'>36</a>
-<a id='n37' href='#n37'>37</a>
-<a id='n38' href='#n38'>38</a>
-<a id='n39' href='#n39'>39</a>
-<a id='n40' href='#n40'>40</a>
-<a id='n41' href='#n41'>41</a>
-<a id='n42' href='#n42'>42</a>
-<a id='n43' href='#n43'>43</a>
-<a id='n44' href='#n44'>44</a>
-<a id='n45' href='#n45'>45</a>
-<a id='n46' href='#n46'>46</a>
-<a id='n47' href='#n47'>47</a>
-<a id='n48' href='#n48'>48</a>
-<a id='n49' href='#n49'>49</a>
-<a id='n50' href='#n50'>50</a>
-<a id='n51' href='#n51'>51</a>
-<a id='n52' href='#n52'>52</a>
-<a id='n53' href='#n53'>53</a>
-<a id='n54' href='#n54'>54</a>
-<a id='n55' href='#n55'>55</a>
-<a id='n56' href='#n56'>56</a>
-<a id='n57' href='#n57'>57</a>
-<a id='n58' href='#n58'>58</a>
-<a id='n59' href='#n59'>59</a>
-<a id='n60' href='#n60'>60</a>
-<a id='n61' href='#n61'>61</a>
-<a id='n62' href='#n62'>62</a>
-<a id='n63' href='#n63'>63</a>
-<a id='n64' href='#n64'>64</a>
-<a id='n65' href='#n65'>65</a>
-<a id='n66' href='#n66'>66</a>
-<a id='n67' href='#n67'>67</a>
-<a id='n68' href='#n68'>68</a>
-<a id='n69' href='#n69'>69</a>
-<a id='n70' href='#n70'>70</a>
-<a id='n71' href='#n71'>71</a>
-<a id='n72' href='#n72'>72</a>
-<a id='n73' href='#n73'>73</a>
-<a id='n74' href='#n74'>74</a>
-<a id='n75' href='#n75'>75</a>
-<a id='n76' href='#n76'>76</a>
-<a id='n77' href='#n77'>77</a>
-<a id='n78' href='#n78'>78</a>
-<a id='n79' href='#n79'>79</a>
-<a id='n80' href='#n80'>80</a>
-<a id='n81' href='#n81'>81</a>
-<a id='n82' href='#n82'>82</a>
-<a id='n83' href='#n83'>83</a>
-<a id='n84' href='#n84'>84</a>
-<a id='n85' href='#n85'>85</a>
-<a id='n86' href='#n86'>86</a>
-<a id='n87' href='#n87'>87</a>
-<a id='n88' href='#n88'>88</a>
-<a id='n89' href='#n89'>89</a>
-<a id='n90' href='#n90'>90</a>
-<a id='n91' href='#n91'>91</a>
-<a id='n92' href='#n92'>92</a>
-<a id='n93' href='#n93'>93</a>
-<a id='n94' href='#n94'>94</a>
-<a id='n95' href='#n95'>95</a>
-<a id='n96' href='#n96'>96</a>
-<a id='n97' href='#n97'>97</a>
-<a id='n98' href='#n98'>98</a>
-<a id='n99' href='#n99'>99</a>
-<a id='n100' href='#n100'>100</a>
-<a id='n101' href='#n101'>101</a>
-<a id='n102' href='#n102'>102</a>
-<a id='n103' href='#n103'>103</a>
-<a id='n104' href='#n104'>104</a>
-<a id='n105' href='#n105'>105</a>
-<a id='n106' href='#n106'>106</a>
-<a id='n107' href='#n107'>107</a>
-<a id='n108' href='#n108'>108</a>
-<a id='n109' href='#n109'>109</a>
-<a id='n110' href='#n110'>110</a>
-<a id='n111' href='#n111'>111</a>
-<a id='n112' href='#n112'>112</a>
-<a id='n113' href='#n113'>113</a>
-<a id='n114' href='#n114'>114</a>
-<a id='n115' href='#n115'>115</a>
-<a id='n116' href='#n116'>116</a>
-<a id='n117' href='#n117'>117</a>
-<a id='n118' href='#n118'>118</a>
-<a id='n119' href='#n119'>119</a>
-<a id='n120' href='#n120'>120</a>
-<a id='n121' href='#n121'>121</a>
-<a id='n122' href='#n122'>122</a>
-<a id='n123' href='#n123'>123</a>
-<a id='n124' href='#n124'>124</a>
-<a id='n125' href='#n125'>125</a>
-<a id='n126' href='#n126'>126</a>
-<a id='n127' href='#n127'>127</a>
-<a id='n128' href='#n128'>128</a>
-<a id='n129' href='#n129'>129</a>
-<a id='n130' href='#n130'>130</a>
-<a id='n131' href='#n131'>131</a>
-<a id='n132' href='#n132'>132</a>
-<a id='n133' href='#n133'>133</a>
-<a id='n134' href='#n134'>134</a>
-<a id='n135' href='#n135'>135</a>
-<a id='n136' href='#n136'>136</a>
-<a id='n137' href='#n137'>137</a>
-<a id='n138' href='#n138'>138</a>
-<a id='n139' href='#n139'>139</a>
-<a id='n140' href='#n140'>140</a>
-<a id='n141' href='#n141'>141</a>
-<a id='n142' href='#n142'>142</a>
-<a id='n143' href='#n143'>143</a>
-<a id='n144' href='#n144'>144</a>
-<a id='n145' href='#n145'>145</a>
-<a id='n146' href='#n146'>146</a>
-<a id='n147' href='#n147'>147</a>
-<a id='n148' href='#n148'>148</a>
-<a id='n149' href='#n149'>149</a>
-<a id='n150' href='#n150'>150</a>
-<a id='n151' href='#n151'>151</a>
-<a id='n152' href='#n152'>152</a>
-<a id='n153' href='#n153'>153</a>
-<a id='n154' href='#n154'>154</a>
-<a id='n155' href='#n155'>155</a>
-<a id='n156' href='#n156'>156</a>
-<a id='n157' href='#n157'>157</a>
-<a id='n158' href='#n158'>158</a>
-<a id='n159' href='#n159'>159</a>
-<a id='n160' href='#n160'>160</a>
-<a id='n161' href='#n161'>161</a>
-<a id='n162' href='#n162'>162</a>
-<a id='n163' href='#n163'>163</a>
-<a id='n164' href='#n164'>164</a>
-<a id='n165' href='#n165'>165</a>
-<a id='n166' href='#n166'>166</a>
-<a id='n167' href='#n167'>167</a>
-<a id='n168' href='#n168'>168</a>
-<a id='n169' href='#n169'>169</a>
-<a id='n170' href='#n170'>170</a>
-<a id='n171' href='#n171'>171</a>
-<a id='n172' href='#n172'>172</a>
-<a id='n173' href='#n173'>173</a>
-<a id='n174' href='#n174'>174</a>
-<a id='n175' href='#n175'>175</a>
-<a id='n176' href='#n176'>176</a>
-<a id='n177' href='#n177'>177</a>
-<a id='n178' href='#n178'>178</a>
-<a id='n179' href='#n179'>179</a>
-<a id='n180' href='#n180'>180</a>
-<a id='n181' href='#n181'>181</a>
-<a id='n182' href='#n182'>182</a>
-<a id='n183' href='#n183'>183</a>
-<a id='n184' href='#n184'>184</a>
-<a id='n185' href='#n185'>185</a>
-<a id='n186' href='#n186'>186</a>
-<a id='n187' href='#n187'>187</a>
-<a id='n188' href='#n188'>188</a>
-<a id='n189' href='#n189'>189</a>
-<a id='n190' href='#n190'>190</a>
-<a id='n191' href='#n191'>191</a>
-<a id='n192' href='#n192'>192</a>
-<a id='n193' href='#n193'>193</a>
-<a id='n194' href='#n194'>194</a>
-<a id='n195' href='#n195'>195</a>
-<a id='n196' href='#n196'>196</a>
-<a id='n197' href='#n197'>197</a>
-<a id='n198' href='#n198'>198</a>
-<a id='n199' href='#n199'>199</a>
-<a id='n200' href='#n200'>200</a>
-<a id='n201' href='#n201'>201</a>
-<a id='n202' href='#n202'>202</a>
-<a id='n203' href='#n203'>203</a>
-<a id='n204' href='#n204'>204</a>
-<a id='n205' href='#n205'>205</a>
-<a id='n206' href='#n206'>206</a>
-<a id='n207' href='#n207'>207</a>
-<a id='n208' href='#n208'>208</a>
-<a id='n209' href='#n209'>209</a>
-<a id='n210' href='#n210'>210</a>
-<a id='n211' href='#n211'>211</a>
-<a id='n212' href='#n212'>212</a>
-<a id='n213' href='#n213'>213</a>
-<a id='n214' href='#n214'>214</a>
-<a id='n215' href='#n215'>215</a>
-<a id='n216' href='#n216'>216</a>
-<a id='n217' href='#n217'>217</a>
-<a id='n218' href='#n218'>218</a>
-<a id='n219' href='#n219'>219</a>
-<a id='n220' href='#n220'>220</a>
-<a id='n221' href='#n221'>221</a>
-<a id='n222' href='#n222'>222</a>
-<a id='n223' href='#n223'>223</a>
-<a id='n224' href='#n224'>224</a>
-<a id='n225' href='#n225'>225</a>
-<a id='n226' href='#n226'>226</a>
-<a id='n227' href='#n227'>227</a>
-<a id='n228' href='#n228'>228</a>
-<a id='n229' href='#n229'>229</a>
-<a id='n230' href='#n230'>230</a>
-<a id='n231' href='#n231'>231</a>
-<a id='n232' href='#n232'>232</a>
-<a id='n233' href='#n233'>233</a>
-<a id='n234' href='#n234'>234</a>
-<a id='n235' href='#n235'>235</a>
-<a id='n236' href='#n236'>236</a>
-<a id='n237' href='#n237'>237</a>
-<a id='n238' href='#n238'>238</a>
-<a id='n239' href='#n239'>239</a>
-<a id='n240' href='#n240'>240</a>
-<a id='n241' href='#n241'>241</a>
-<a id='n242' href='#n242'>242</a>
-<a id='n243' href='#n243'>243</a>
-<a id='n244' href='#n244'>244</a>
-<a id='n245' href='#n245'>245</a>
-<a id='n246' href='#n246'>246</a>
-<a id='n247' href='#n247'>247</a>
-<a id='n248' href='#n248'>248</a>
-<a id='n249' href='#n249'>249</a>
-<a id='n250' href='#n250'>250</a>
-<a id='n251' href='#n251'>251</a>
-<a id='n252' href='#n252'>252</a>
-<a id='n253' href='#n253'>253</a>
-<a id='n254' href='#n254'>254</a>
-<a id='n255' href='#n255'>255</a>
-<a id='n256' href='#n256'>256</a>
-<a id='n257' href='#n257'>257</a>
-<a id='n258' href='#n258'>258</a>
-<a id='n259' href='#n259'>259</a>
-<a id='n260' href='#n260'>260</a>
-<a id='n261' href='#n261'>261</a>
-<a id='n262' href='#n262'>262</a>
-<a id='n263' href='#n263'>263</a>
-<a id='n264' href='#n264'>264</a>
-<a id='n265' href='#n265'>265</a>
-<a id='n266' href='#n266'>266</a>
-<a id='n267' href='#n267'>267</a>
-<a id='n268' href='#n268'>268</a>
-<a id='n269' href='#n269'>269</a>
-<a id='n270' href='#n270'>270</a>
-<a id='n271' href='#n271'>271</a>
-<a id='n272' href='#n272'>272</a>
-<a id='n273' href='#n273'>273</a>
-<a id='n274' href='#n274'>274</a>
-<a id='n275' href='#n275'>275</a>
-<a id='n276' href='#n276'>276</a>
-<a id='n277' href='#n277'>277</a>
-<a id='n278' href='#n278'>278</a>
-<a id='n279' href='#n279'>279</a>
-<a id='n280' href='#n280'>280</a>
-<a id='n281' href='#n281'>281</a>
-<a id='n282' href='#n282'>282</a>
-<a id='n283' href='#n283'>283</a>
-<a id='n284' href='#n284'>284</a>
-<a id='n285' href='#n285'>285</a>
-<a id='n286' href='#n286'>286</a>
-<a id='n287' href='#n287'>287</a>
-<a id='n288' href='#n288'>288</a>
-<a id='n289' href='#n289'>289</a>
-<a id='n290' href='#n290'>290</a>
-</pre></td>
-<td class='lines'><pre><code>#ifndef __khrplatform_h_
+#ifndef __khrplatform_h_
 #define __khrplatform_h_
 
 /*
@@ -371,7 +43,7 @@
  *    http://www.khronos.org/registry/implementers_guide.pdf
  *
  * This file should be included as
- *        #include &lt;KHR/khrplatform.h&gt;
+ *        #include <KHR/khrplatform.h>
  * by Khronos client API header files that use its types and defines.
  *
  * The types in khrplatform.h should only be used to define API-specific types.
@@ -418,7 +90,7 @@
  *                                  int arg2) KHRONOS_APIATTRIBUTES;
  */
 
-#if defined(__SCITECH_SNAP__) &amp;&amp; !defined(KHRONOS_STATIC)
+#if defined(__SCITECH_SNAP__) && !defined(KHRONOS_STATIC)
 #   define KHRONOS_STATIC 1
 #endif
 
@@ -447,7 +119,7 @@
  * This follows the return type of the function  and precedes the function
  * name in the function prototype.
  */
-#if defined(_WIN32) &amp;&amp; !defined(_WIN32_WCE) &amp;&amp; !defined(KHRONOS_STATIC)
+#if defined(_WIN32) && !defined(_WIN32_WCE) && !defined(KHRONOS_STATIC)
     /* Win32 but not WinCE */
 #   define KHRONOS_APIENTRY __stdcall
 #else
@@ -468,13 +140,13 @@
 /*-------------------------------------------------------------------------
  * basic type definitions
  *-----------------------------------------------------------------------*/
-#if (defined(__STDC_VERSION__) &amp;&amp; __STDC_VERSION__ &gt;= 199901L) || defined(__GNUC__) || defined(__SCO__) || defined(__USLC__)
+#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__GNUC__) || defined(__SCO__) || defined(__USLC__)
 
 
 /*
- * Using &lt;stdint.h&gt;
+ * Using <stdint.h>
  */
-#include &lt;stdint.h&gt;
+#include <stdint.h>
 typedef int32_t                 khronos_int32_t;
 typedef uint32_t                khronos_uint32_t;
 typedef int64_t                 khronos_int64_t;
@@ -485,9 +157,9 @@ typedef uint64_t                khronos_uint64_t;
 #elif defined(__VMS ) || defined(__sgi)
 
 /*
- * Using &lt;inttypes.h&gt;
+ * Using <inttypes.h>
  */
-#include &lt;inttypes.h&gt;
+#include <inttypes.h>
 typedef int32_t                 khronos_int32_t;
 typedef uint32_t                khronos_uint32_t;
 typedef int64_t                 khronos_int64_t;
@@ -495,7 +167,7 @@ typedef uint64_t                khronos_uint64_t;
 #define KHRONOS_SUPPORT_INT64   1
 #define KHRONOS_SUPPORT_FLOAT   1
 
-#elif defined(_WIN32) &amp;&amp; !defined(__SCITECH_SNAP__)
+#elif defined(_WIN32) && !defined(__SCITECH_SNAP__)
 
 /*
  * Win32
@@ -539,7 +211,7 @@ typedef unsigned int            khronos_uint32_t;
 /*
  * Generic fallback
  */
-#include &lt;stdint.h&gt;
+#include <stdint.h>
 typedef int32_t                 khronos_int32_t;
 typedef uint32_t                khronos_uint32_t;
 typedef int64_t                 khronos_int64_t;
@@ -616,9 +288,3 @@ typedef enum {
 } khronos_boolean_enum_t;
 
 #endif /* __khrplatform_h_ */
-</code></pre></td></tr></table>
-</div> <!-- class=content -->
-<div class='footer'>generated by <a href='https://git.zx2c4.com/cgit/about/'>cgit v1.2.1</a> (<a href='https://git-scm.com/'>git 2.18.0</a>) at 2022-06-15 01:54:31 +0000</div>
-</div> <!-- id=cgit -->
-</body>
-</html>
