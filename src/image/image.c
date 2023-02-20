3abf79fa0cea3729c1e99af9c1f44d29e4d007f4
sanine <sanine.not@pm.me>
2023-02-19 19:18:15 -0600

    remove cairo include in image.c

--- src/image/image.c
+++ src/image/image.c
@@ -1,6 +1,5 @@
 #include <lua.h>
 #include <honeysuckle.h>
-#include <cairo/cairo.h>
 /* assimp provides its own stb_image implementation */
 /*#define STB_IMAGE_IMPLEMENTATION*/
 #include "stb_image.h"
